9f928af22795a2245653823f956e784fb61b2c10
Hossein <hosseinebi558yaru@gmail.com>
2024-09-18 16:26:32 +0330

    1-- Process-Change In platform-اصلاح درخواست.groovy

---
+++ Archive/1-- Process-Change In platform-اصلاح درخواست.groovy
@@ -0,0 +1,19 @@
+import com.onresolve.scriptrunner.db.DatabaseUtil
+//checks for status. if the status is 'اصلاح درخواست' editor can edit field
+// def issue = Issues.getByKey('RAYMON-12')
+/*
+def status = issue.status.name.toString()
+
+
+log.warn(status)
+
+if (status == 'اصلاح درخواست') {
+  issue.update {
+    setCustomFieldValue('Editors', issue.reporter)
+  }
+} else {
+  issue.update {
+    setCustomFieldValue('Editors', '')
+  }
+}
+*/
