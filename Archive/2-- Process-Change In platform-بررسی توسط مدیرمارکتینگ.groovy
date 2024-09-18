da1744dbda29067464f320871451494a29e3581e
Hossein <hosseinebi558yaru@gmail.com>
2024-09-18 16:29:12 +0330

    2-- Process-Change In platform-بررسی توسط مدیرمارکتینگ.groovy

---
+++ Archive/2-- Process-Change In platform-بررسی توسط مدیرمارکتینگ.groovy
@@ -0,0 +1,22 @@
+// Unable to add package declaration. The parent folder names contain identifiers that are not valid in a package name.
+
+import com.onresolve.scriptrunner.db.DatabaseUtil
+
+// def issue = Issues.getByKey('RAYMON-4')
+/*
+def status = issue.status.name.toString()
+def creator = issue.creator.username.toString()
+
+
+log.warn(status)
+
+if (status == 'اصلاح درخواست') {
+  issue.update {
+    setCustomFieldValue('Editors', issue.reporter)
+  }
+} else {
+  issue.update {
+    setCustomFieldValue('Editors', '')
+  }
+}
+*/
