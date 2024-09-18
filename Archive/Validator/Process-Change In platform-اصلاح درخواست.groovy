5e5fb14a6fe9ee5da8cb73a9e374ba61348cd271
Hossein <hosseinebi558yaru@gmail.com>
2024-09-18 16:42:17 +0330

    Process-Change In platform-اصلاح درخواست.groovy

---
+++ Archive/Validator/Process-Change In platform-اصلاح درخواست.groovy
@@ -0,0 +1,44 @@
+import org.ofbiz.core.entity.jdbc.DatabaseUtil
+import com.opensymphony.workflow.InvalidInputException
+import com.atlassian.jira.component.ComponentAccessor
+import com.onresolve.scriptrunner.db.DatabaseUtil
+import groovy.sql.Sql
+
+// def issue = Issues.getByKey('RAYMON-2')
+
+def currentUser = Users.getLoggedInUser()
+def creator = issue.creator.username.toString()
+log.warn(creator)
+def isingroup = ComponentAccessor.getGroupManager().getGroupsForUser(currentUser)
+
+def qry_manager = """
+  SELECT [managername] FROM [Jira].[Raymon].[managerchart] where [employee] =?
+
+
+"""
+
+
+if (isingroup?.find {(it.name == "Managers")}) 
+{
+    log.warn("issuekey "+issue.getKey()+" IS manager "+creator)
+
+}
+else 
+{
+    DatabaseUtil.withSql('Jira-DB') { sql ->
+        def result = sql.rows(qry_manager,creator).toString()
+        result = result.substring(result.indexOf(":")+1,result.length()-2)
+        throw new InvalidInputException("${issue.reporter.displayName.toString()} ، این وظیفه در انتظار تعیین وضعیت است. در صورت نیاز به اصلاح ${result} آن را در وضعیت اصلاح قرار خواهد داد.")
+
+
+
+
+    }
+
+ 
+
+ 
+}
+
+
+
